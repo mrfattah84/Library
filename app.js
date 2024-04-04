37185e92f22ed39244acc3f354c3fc28e0e89f38
mrfattah84 <148151611+mrfattah84@users.noreply.github.com>
2024-04-05 00:34:46 +0330

    done

--- app.js
+++ app.js
@@ -1,21 +1,77 @@
-let removebtn = document.querySelectorAll('.remove');
-removebtn.forEach(btn=>btn.addEventListener('click', removeBook));
+let books = [];
+function loadBooks(books){
+    let booksContainer = document.querySelector('.booksContainer');
+    booksContainer.innerHTML = '';
+    books.forEach((book) => {
+        let bookElement= `
+        <div class="book">
+            <button class="remove"><i class="material-icons">cancel</i></button>
+            <h3 class="title">${book.title}</h3>
+            <p class="author">Author: ${book.author}</p>
+            <p class="pages">Number of pages: ${book.pages}</p>
+        `;
+        if (book.read) {
+            bookElement += `
+            <p class="green status"><button class="statusChange"><i class="green material-icons">check_circle</i></button><span class="statusText">Already read</span></p>
+        </div>`;
+        }
+        else{
+            bookElement += `
+            <p class="red status"><button class="statusChange"><i class="grey material-icons">check_circle</i></button><span class="statusText">Haven't read yet</span></p>
+        </div>`;
+        }
+        booksContainer.innerHTML += bookElement;
+    });
+    let removebtn = document.querySelectorAll('.remove');
+    removebtn.forEach(btn=>btn.addEventListener('click', removeBook));
+    let statusbtn = document.querySelectorAll('.statusChange');
+    statusbtn.forEach(btn=>btn.addEventListener('click', changeStatus));
+}
+loadBooks(books);
+
 function removeBook(){
-    this.parentElement.remove();
+    for (let i = 0; i < books.length; i++) {
+        const book = books[i];
+        if (book.title == this.parentElement.querySelector('.title').innerText) {
+            books.splice(i,1);
+        }
+    }
+    loadBooks(books);
 }
 
-let statusbtn = document.querySelectorAll('.statusChange');
-statusbtn.forEach(btn=>btn.addEventListener('click', changeStatus));
 function changeStatus(){
-    console.log(this.parentElement.querySelector('.statusText').innerText)
-    if (this.innerHTML == '<i class="green material-icons">check_circle</i>') {
-        this.parentElement.className = "red status";
-        this.innerHTML = '<i class="grey material-icons">check_circle</i>';
-        this.parentElement.querySelector('.statusText').innerText = "Haven't read yet"
+    for (let i = 0; i < books.length; i++) {
+        const book = books[i];
+        if (book.title == this.parentElement.parentElement.querySelector('.title').innerText) {
+            books[i].read = !books[i].read;
+        }
     }
-    else if (this.innerHTML == '<i class="grey material-icons">check_circle</i>') {
-        this.parentElement.className = "green status";
-        this.innerHTML = '<i class="green material-icons">check_circle</i>';
-        this.parentElement.querySelector('.statusText').innerText = "Already read"
-    }
+    loadBooks(books);
 }
+
+const dialog = document.querySelector('dialog');
+const form = document.querySelector('form');
+const addBookbtn = document.querySelector('.AddBook');
+const closeDialogbtn = document.querySelector('.close');
+addBookbtn.addEventListener('click', () => {
+    dialog.showModal();
+    form.reset();
+});
+closeDialogbtn.addEventListener('click', () => dialog.close());
+
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const read = document.getElementById('read');
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(){
+    books.push({
+        'title' : title.value,
+        'author' : author.value,
+        'pages' : pages.value,
+        'read' : read.checked
+    })
+    loadBooks(books);
+}
+
